28bd7d0ab29329e7ccf4262624c58630e860acec
Reversep <adilet.t.k@gmail.com>
2023-05-05 21:25:52 +0600

    changed db for kg volunteer and settings

--- shelter/settings.py
+++ shelter/settings.py
@@ -130,11 +130,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-
-]
+CORS_ALLOW_ALL_ORIGINS = True
